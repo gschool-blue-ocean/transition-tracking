269794fc0f2cd3f8105424c8c27ae4f110651b12
Shu <luo.s@outlook.com>
2023-07-06 12:06:35 -0800

    working localhost db

--- api/src/db/migration.sql
+++ api/src/db/migration.sql
@@ -1,6 +1,29 @@
-DROP TABLE IF EXISTS tasks;
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS cohort;
 
-CREATE TABLE tasks (
+-- Create the cohort table
+CREATE TABLE cohort (
   id SERIAL PRIMARY KEY,
-  description TEXT
+  cohort_name VARCHAR(255),
+  instructor VARCHAR(255),
+  start_date DATE,
+  end_date DATE
 );
+
+-- Create the student table with a foreign key referencing cohort_name in cohort table
+CREATE TABLE student (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(255),
+  branch VARCHAR(255),
+  status VARCHAR(255),
+  ets DATE,
+  linkedin VARCHAR(255),
+  github VARCHAR(255),
+  comment TEXT,
+  cohort_name VARCHAR(255) NOT NULL
+);
+
--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,15 @@ import app from "./server.js";
 
 const PORT = process.env.PORT;
 
+app.get("/api/mcsp/:id", async (req, res) => {
+  const id = req.params.id;
+  sql`SELECT * FROM cohort JOIN student ON cohort.cohort_name = student.cohort_name WHERE cohort.cohort_name = mcsp${id}`.then(
+    (rows) => {
+      res.send(rows[0]);
+    }
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
