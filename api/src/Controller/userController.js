ee9a8968ab396f21f989b5e8888d3f530bd33590
Phillip Sussman <phillipsussman@gmail.com>
2023-07-07 09:18:43 -0700

    first commit

--- api/src/Controller/userController.js
+++ api/src/Controller/userController.js
@@ -1,53 +1,43 @@
 import { db } from "../db/database.js";
-import { 
-    allTasks, 
-    speTask,
-    addingTask, 
-    deletingTask } from "./queries.js";
+import { allCohorts, speCohort, addCohort, deleteCohort } from "./queries.js";
 
-export const getAllTasks = async (req, res) => {
-    try{
-      const result = await db.query(allTasks);
-      res.send(result.rows);
+export const getAllCohorts = async (req, res) => {
+	try {
+		const result = await db.query(allCohorts);
+		res.send(result.rows);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
 
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    };
-  };
-  
-export const getSpeTask =  async (req, res) => {
-  try {
-    const id = Number(req.params.id);
-    const result = await db.query(speTask, [id]);
-      res.send(result.rows[0]);
+export const getSpeCohort = async (req, res) => {
+	try {
+		const id = Number(req.params.id);
+		const result = await db.query(speCohort, [id]);
+		res.send(result.rows[0]);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
 
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    };
-  };
-  
- export const addTasks = async (req, res) => {
-    try{
-      const { description } = req.body;
-    
-      const result = await db.query(addingTask, [description])
-      res.send(result.rows[0]);
+export const addingCohort = async (req, res) => {
+	try {
+		const { description } = req.body;
 
-    } catch(error) {
-      res.status(500).json({ error: error.message });
-    };
-  };
-  
-export const delTask =  async (req, res) => {
-    try{
-      const id = Number(req.params.id);
-    
-      await db.query(deletingTask, [id]).catch(next);
-      res.sendStatus(204);
+		const result = await db.query(addCohort, [description]);
+		res.send(result.rows[0]);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
 
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
-  
+export const delCohort = async (req, res) => {
+	try {
+		const id = Number(req.params.id);
 
+		await db.query(deleteCohort, [id]).catch(next);
+		res.sendStatus(204);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
--- api/src/db/migration.sql
+++ api/src/db/migration.sql
@@ -1,6 +1,25 @@
-DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS cohort;
+DROP TABLE IF EXISTS students;
 
-CREATE TABLE tasks (
+CREATE TABLE cohort (
   id SERIAL PRIMARY KEY,
-  description TEXT
+  cohort_id VARCHAR(50),
+  instructor VARCHAR(80),
+  start DATE,
+  end DATE
+);
+
+CREATE TABLE students (
+  id SERIAL FOREIGN KEY,
+  first_name VARCHAR(80),
+  last_name VARCHAR(80),
+  email VARCHAR(80),
+  phone VARCHAR(80),
+  branch VARCHAR(80),
+  status VARCHAR(80),
+  ETS DATE,
+  linkedin VARCHAR(80),
+  github VARCHAR(80),
+  comment TEXT,
+  cohort_id VARCHAR(80)
 );
