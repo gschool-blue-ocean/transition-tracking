83685d9181312063575ea4f646ce39a92b8bd503
Fitz Gerald <gfitzsicat@gmail,com>
2023-07-11 13:12:24 -0400

    a

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -7,55 +7,13 @@ import Footer from "../Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  const [cohorts, setCohorts] = useState([]);
-	const [students, setStudents] = useState([]);
-	const [filter, setFilter] = useState("");
-	const [selectedCohort, setSelectedCohort] = useState("");
-
-  
-  useEffect(() => {
-    const getCohorts = async () => {
-      try{
-          const resultCo =  await axios.get("/api/cohort")
-          setCohorts(resultCo.data);
-      } catch (error) {
-        console.error(error)
-      };
-    };
-    getCohorts();
-  }, []);
-
-  useEffect(() => {
-      const getStudents = async() => {
-      try{
-         const resultStu = await axios.get("/api/students")
-         setStudents(resultStu.data);
-      } catch (error) {
-        console.error(error)
-      };
-    };
-    getStudents();
-  }, []);
-
-
-	const handleFilterChange = (e) => {
-		setFilter(e.target.value);
-		setSelectedCohort(e.target.value);
-	};
-
-
   return (
     <div className="App">
     <Router>
       <Navbar />
         <Routes>
           <Route path="/" exact element={<LandingPage /> }/>
-          <Route path="/mcsp" exact element={<Mcsp 
-                handleFilterChang={handleFilterChange}
-                setCohorts={setCohorts} 
-                setStudents={setStudents}
-                />}
-           />
+          <Route path="/mcsp" exact element={<Mcsp />} />
         </Routes>
       <Footer />
     </Router>
