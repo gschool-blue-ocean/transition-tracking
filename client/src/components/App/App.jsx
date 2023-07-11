0a6db48e9ea57e1f726602bc90a625c752e60449
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-07-11 17:16:05 -0400

    Added Dockerfile.prod for deployment, added Student Component

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -7,19 +7,48 @@ import Footer from "../Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
-    return (
-      <div className="App">
+  const [students, setStudents] = useState([]);
+  const [cohorts, setCohorts] = useState([]);
+
+  useEffect(() => {
+    const getCohrts = async () => {
+      try {
+        const response = await axios.get(`/api/cohorts`);
+        setCohorts(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getCohorts();
+  }, []);
+
+  useEffect(() => {
+    const getStudents = async (id) => {
+      try {
+        const response = await axios.get(`/api/cohort/${id}`);
+        if (response.length > 0) {
+          setStudents(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    getStudents();
+  }, []);
+
+  return (
+    <div className="App">
       <Router>
         <Navbar />
-          <Routes>
-            <Route path="/" exact element={<LandingPage /> }/>
-            <Route path="/mcsp" exact element={<Mcsp />} />
-          </Routes>
+        <Routes>
+          <Route path="/" exact element={<LandingPage />} />
+          <Route path="/mcsp" exact element={<Mcsp students={students} />} />
+        </Routes>
         <Footer />
       </Router>
-      </div>
-    );
+    </div>
+  );
 };
-
 
 export default App;
