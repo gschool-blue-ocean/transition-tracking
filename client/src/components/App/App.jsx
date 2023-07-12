f46515a9e25d09141ca5eada39d611092f879e07
Fitz Gerald <gfitzsicat@gmail,com>
2023-07-11 23:39:22 -0400

    ..

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -7,6 +7,7 @@ import Footer from "../Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
+
     return (
       <div className="App">
       <Router>
