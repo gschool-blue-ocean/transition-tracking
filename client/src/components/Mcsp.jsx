a16e1fe60918f03e7498edfbad7da3a3bb7218f3
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-07-12 15:03:34 -0400

    added Dashboard component w/out styling

--- client/src/components/Mcsp.jsx
+++ client/src/components/Mcsp.jsx
@@ -4,6 +4,7 @@ import Students from "./Students";
 export default function Mcsp({ students }) {
   return (
     <div>
+      <Dashboard students={students} />
       <Students students={students} />
     </div>
   );
