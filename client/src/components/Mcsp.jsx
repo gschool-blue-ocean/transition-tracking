d42b4779e4beb5f2422477e07eece36f9f6a0f18
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-07-14 14:42:08 -0400

    color circles added to student card

--- client/src/components/Mcsp.jsx
+++ client/src/components/Mcsp.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import Students from "./Students";
 import Navbar from "./Navbar";
-import '../styles/Mcsp.css'
+import Dashboard from "./Dashboard";
+import "../styles/Mcsp.css";
 
-
-
-
-export default function Mcsp({ cohort, students, setStudents, isDDOpen, setIsDDOpen }) {
+export default function Mcsp({
+  cohort,
+  students,
+  setStudents,
+  isDDOpen,
+  setIsDDOpen,
+}) {
   return (
-
-
     <div className="app">
-      <Navbar cohort={cohort}
-               students={students}
-               setStudents={setStudents}
-               isDDOpen={isDDOpen}
-               setIsDDOpen={setIsDDOpen}/>
-        <Students students={students} />
-        </div>
-
+      <Navbar
+        cohort={cohort}
+        students={students}
+        setStudents={setStudents}
+        isDDOpen={isDDOpen}
+        setIsDDOpen={setIsDDOpen}
+      />
+      <Dashboard students={students} />
+      <Students students={students} />
+    </div>
   );
 }
--- client/src/components/Students.jsx
+++ client/src/components/Students.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Modal from "./Modal";
-import '../styles/Students.css'
+import "../styles/Students.css";
 
 const Student = ({ students, setStudents }) => {
   const [showModal, setShowModal] = useState(false);
   console.log("from students: ", setStudents);
-  // student.branch
-  // Tayla's added code with html
 
   const openModal = () => {
     console.log("Modal Opened");
@@ -19,44 +17,58 @@ const Student = ({ students, setStudents }) => {
     setShowModal(false);
   };
 
-  
-    const renderStatus = (status) => {
-      
-      if (status === "within 6 months prior ETS") {
-        return "In Process";
-      } else if (status === "Seperated") {
-        return "Separated";
-      } else if (status === "more than 6 months prior ETS") {
-        return "Skillbridge";
-      } else {
-        return "";
-      }
-    };
-
+  const renderStatus = (status) => {
+    if (status === "within 6 months prior ETS") {
+      return {
+        text: "In Process",
+        color: "yellow",
+      };
+    } else if (status === "Seperated") {
+      return {
+        text: "Seperated",
+        color: "green",
+      };
+    } else if (status === "more than 6 months prior ETS") {
+      return {
+        text: "Skillbridge",
+        color: "red",
+      };
+    } else {
+      return {
+        text: "",
+        color: "",
+      };
+    }
+  };
 
   return (
     <div className="students">
       {students.map((student) => (
         <div key={student.id} className="student">
-            <div className="student-head">
+          <div className="student-head">
             <h2 className="name">
-            {student.first_name} {student.last_name}
+              {student.first_name} {student.last_name}
             </h2>
-            <p className="status">{renderStatus(student.status)}</p>
+            <p className="status">
+              {renderStatus(student.status).text}
+              <div
+                className="color-circle"
+                style={{ backgroundColor: renderStatus(student.status).color }}
+              ></div>
+            </p>
           </div>
-          <br/>
+          <br />
           <p>{student.branch}</p>
           <p>ETS: {student.ets.split("T")[0]}</p>
-          <br/>
+          <br />
           <p className="contact">Contact:</p>
           <p>{student.email}</p>
           <p>{student.phone}</p>
 
-
-          {/* 
+          {/*
           <p>LinkedIn: {student.linkedin}</p>
           <p>GitHub: {student.github}</p> */}
-          
+
           {/* <div className="comment">
             {Object.entries(student.comment)
               ? Object.entries(student.comment).map(([key, value]) => (
