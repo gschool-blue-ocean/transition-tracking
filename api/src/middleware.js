27bd9ae8c2d758097227ae66c8a5302d2a59b680
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-07-17 17:47:40 -0400

    added middleware to middleware.js

--- api/src/middleware.js
+++ api/src/middleware.js
@@ -6,5 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", router);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'none'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;"
+  );
+  next();
+});
 
 export default app;
