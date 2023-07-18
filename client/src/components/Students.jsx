85447d2dd62a20f00112b614446f9f642e110993
Shu <luo.s@outlook.com>
2023-07-18 09:26:00 -0800

    working backend

--- client/src/components/Students.jsx
+++ client/src/components/Students.jsx
@@ -4,33 +4,56 @@ import Modal from "./Modal";
 
 const Student = ({ students, setStudents }) => {
   const [showModal, setShowModal] = useState(false);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+  // console.log("from students: ", setStudents);
   // student.branch
   // Tayla's added code with html
 
-  const openModal = () => {
+  const openModal = (studentId) => {
     console.log("Modal Opened");
     setShowModal(true);
+    setSelectedStudent(studentId);
   };
 
   const closeModal = () => {
     console.log("Modal closed");
     setShowModal(false);
+    setSelectedStudent(null);
+  };
+
+  const renderStatus = (status) => {
+    if (status === "within 6 months prior ETS") {
+      return "In Process";
+    } else if (status === "Seperated") {
+      return "Separated";
+    } else if (status === "more than 6 months prior ETS") {
+      return "Skillbridge";
+    } else {
+      return "";
+    }
   };
 
   return (
     <div className="students">
       {students.map((student) => (
         <div key={student.id} className="student">
-          <h2>
-            {student.first_name} {student.last_name}
-          </h2>
-          <p>Email: {student.email}</p>
-          <p>Phone: {student.phone}</p>
-          <p>Branch: {student.branch}</p>
+          <div className="student-head">
+            <h2 className="name">
+              {student.first_name} {student.last_name}
+            </h2>
+            <p className="status">{renderStatus(student.status)}</p>
+          </div>
+          <br />
+          <p>{student.branch}</p>
           <p>ETS: {student.ets.split("T")[0]}</p>
-          <p>Status: {student.status}</p>
-          <p>LinkedIn: {student.linkedin}</p>
+          <br />
+          <p className="contact">Contact:</p>
+          <p>{student.email}</p>
+          <p>{student.phone}</p>
+
+          {/* <p>LinkedIn: {student.linkedin}</p>
           <p>GitHub: {student.github}</p>
+           */}
           <div className="comment">
             {Object.entries(student.comment)
               ? Object.entries(student.comment).map(([key, value]) => (
@@ -40,8 +63,10 @@ const Student = ({ students, setStudents }) => {
                 ))
               : {}}
           </div>
-          <button onClick={openModal}>|Make a Comment|</button>
-          {showModal && (
+          <button onClick={() => openModal(student.id)}>
+            |Make a Comment|
+          </button>
+          {selectedStudent === student.id && (
             <Modal
               onClose={closeModal}
               studentId={student.id}
