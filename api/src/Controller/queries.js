28f8cc3371c23f221eba8ade627dc9ec0e9bbb38
Phillip Sussman <phillipsussman@gmail.com>
2023-07-18 10:18:54 -0700

    first commit, made a create cohort btn/modal posts to back end

--- api/src/Controller/queries.js
+++ api/src/Controller/queries.js
@@ -2,10 +2,10 @@ export const allCohorts = `SELECT * FROM cohort`;
 
 export const allStudents = `SELECT * FROM students ORDER BY last_name`;
 
-export const postComment = `UPDATE students SET comment = $1 WHERE id = $2 RETURNING *;`;
+export const postComment = `UPDATE students SET comment = $1 WHERE id = $2 RETURNING *`;
 
 export const speCohort = `SELECT * FROM cohort JOIN students ON students.cohort_id = cohort.cohort_id WHERE cohort.cohort_id = $1`;
 
-export const addCohort = `SELECT * FROM cohort WHERE id = $1`;
+export const addCohort = `INSERT INTO cohort(cohort_id, instructor, start_date, end_date) VALUES($1, $2, $3, $4) RETURNING *`;
 
 export const deleteCohort = `DELETE FROM cohort WHERE id = $1`;
--- client/src/components/CreateCohort.jsx
+++ client/src/components/CreateCohort.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import "../styles/CreateCohort.css";
 
-function CreateCohort() {
-  return <div>Create a New Cohort</div>;
-}
+const CreateCohort = ({ setCohort }) => {
+	const [isModalOpen, setModalOpen] = useState(false);
+	const [cohortName, setCohortName] = useState("");
+
+	const openModal = () => {
+		setModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setModalOpen(false);
+	};
+
+	const handleChange = (e) => {
+		setCohortName(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.target);
+
+		// Here you can send the POST request to your backend with the cohortName value
+		// For example, using the fetch API or axios
+		// Remember to handle errors and success scenarios as needed
+		// console.log(formData.get("instructor"));
+
+		axios
+			.post("/api/cohort", {
+				cohort_id: formData.get("cohort"),
+				instructor: formData.get("instructor"),
+				start_date: formData.get("start_date"),
+				end_date: formData.get("end_date"),
+			})
+			.then((response) => {
+				// Handle successful response if needed
+				console.log("Request successful!", response.data);
+
+				setCohort((cohorts) => {
+					return [...cohorts, response.data];
+				});
+			})
+			.catch((error) => {
+				// Handle errors
+				console.error("Error sending POST request:", error);
+			});
+
+		closeModal(); // Close the modal after submitting
+	};
+
+	return (
+		<div>
+			<button className="create-cohort-btn" onClick={openModal}>
+				Create a cohort
+			</button>
+			{isModalOpen && (
+				<div className="modal">
+					<div className="modal-content">
+						<span className="close" onClick={closeModal}>
+							&times;
+						</span>
+						<form onSubmit={handleSubmit}>
+							<label>Cohort Name:</label>
+							<input
+								type="text"
+								placeholder="mcsp-21"
+								className="input-txt"
+								name="cohort"
+								value={cohortName}
+								onChange={handleChange}
+							/>
+							<label>Cohort instructor:</label>
+							<input
+								type="text"
+								className="input-txt"
+								name="instructor"
+								placeholder="Danny Andrews"
+							/>
+
+							<label>Start Date:</label>
+							<input className="input-txt" type="date" name="start_date" />
+
+							<label>End Date:</label>
+							<input className="input-txt" type="date" name="end_date" />
+
+							<button className="submit-btn" type="submit">
+								Submit
+							</button>
+						</form>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default CreateCohort;
