af3a83197aab12da0dff4960e68e2216c1d51cc4
oscarmejia1776 <oscar.mejia17@gmail.com>
2023-07-18 15:58:05 -0400

    corrected styling for the mcsp page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Navbar.css";
 import Dropdown from "./Dropdown";
 
 function Navbar({ cohort, students, setStudents, isDDOpen, setIsDDOpen }) {
-	return (
-		<div>
-			<div className="header">
-				<div className="logo"></div>
-				<div className="nav-bar">
-					<div className="nav-items">
-						<div className="nav-item1">Dashboard</div>
-						<div className="nav-item2">News</div>
-						<div className="nav-item3">Help</div>
-					</div>
-				</div>
-				<div className="btn-group">
-					<button onClick={() => setIsDDOpen((prev) => !prev)}>MCSP..</button>
-					{isDDOpen && (
-						<ul className="dropdown-menu">
-							<Dropdown
-								cohort={cohort}
-								students={students}
-								setStudents={setStudents}
-								isDDOpen={isDDOpen}
-								setIsDDOpen={setIsDDOpen}
-							/>
-						</ul>
-					)}
-				</div>
-			</div>
+  const [activeItem, setActiveItem] = useState("dashboard"); // Initial active item is set to 'dashboard'
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
 
-			<div>
-				<button className="mx-12">Modal</button>
-			</div>
-		</div>
-	);
+  return (
+    <div clasName="header">
+      <div className="nav">
+        <div className="logo"></div>
+        <div className="nav-bar">
+          <div className="nav-items">
+            <div
+              className={`nav-item1 ${
+                activeItem === "dashboard" ? "active" : ""
+              }`}
+              onClick={() => handleItemClick("dashboard")}
+            >
+              Dashboard
+            </div>
+            <div
+              className={`nav-item2 ${activeItem === "news" ? "active" : ""}`}
+              onClick={() => handleItemClick("news")}
+            >
+              News
+            </div>
+            <div
+              className={`nav-item3 ${activeItem === "help" ? "active" : ""}`}
+              onClick={() => handleItemClick("help")}
+            >
+              Help
+            </div>
+          </div>
+        </div>
+        <div className="btn-group">
+          <button onClick={() => setIsDDOpen((prev) => !prev)}>MCSP..</button>
+          {isDDOpen && (
+            <ul className="dropdown-menu">
+              <Dropdown
+                cohort={cohort}
+                students={students}
+                setStudents={setStudents}
+                isDDOpen={isDDOpen}
+                setIsDDOpen={setIsDDOpen}
+              />
+            </ul>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <button className="mx-12">Modal</button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
